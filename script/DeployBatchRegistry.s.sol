dd2c3ed99418ac42d77ef781eec7d5bdb804b5f1
IT Admin <itadmin@sinouar.local>
2026-07-13 03:27:10 +0100

    fix(script): deploy BatchRegistry before Verifier to preserve 0x5Fb... contract address

--- script/DeployBatchRegistry.s.sol
+++ script/DeployBatchRegistry.s.sol
@@ -3,16 +3,19 @@ pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {BatchRegistry} from "../src/BatchRegistry.sol";
+import {Verifier} from "../src/Verifier.sol";
 
 contract DeployBatchRegistry is Script {
     function setUp() public {}
 
-    function run() public returns (BatchRegistry) {
+    function run() public returns (BatchRegistry, Verifier) {
         vm.startBroadcast();
 
         BatchRegistry batchRegistry = new BatchRegistry();
+        Verifier verifier = new Verifier();
 
         vm.stopBroadcast();
-        return batchRegistry;
+        return (batchRegistry, verifier);
     }
 }
+
